b34da1286b56413226a9f50a90569d725ada1b11
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-08-01 12:32:43 -0400

    Added more use cases for Word equality checks.

--- src/test/java/helloworld/WordTest.java
+++ src/test/java/helloworld/WordTest.java
@@ -1,6 +1,7 @@
 package helloworld;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -61,5 +62,53 @@ public class WordTest
         boolean result = word1.equals(word2);
 
         assertEquals(result, true);
+    }
+
+    @Test
+    public void testWordInequalityDifferentWord() throws InvalidCaseException
+    {
+        Word.Builder builder = new Word.Builder();
+
+        Word word1 = builder
+                        .addLetter(new LetterUpperCase('H'))
+                        .addLetter(new LetterLowerCase('e'))
+                        .addLetter(new LetterLowerCase('l'))
+                        .addLetter(new LetterLowerCase('l'))
+                        .addLetter(new LetterLowerCase('o'))
+                        .build();
+
+        builder = new Word.Builder();
+
+        Word word2 = builder
+                        .addLetter(new LetterUpperCase('W'))
+                        .addLetter(new LetterLowerCase('o'))
+                        .addLetter(new LetterLowerCase('r'))
+                        .addLetter(new LetterLowerCase('l'))
+                        .addLetter(new LetterLowerCase('d'))
+                        .build();
+        
+        boolean result = word1.equals(word2);
+
+        assertEquals(result, false);
+    }
+
+    @Test
+    public void testWordInequalityNull() throws InvalidCaseException
+    {
+        Word.Builder builder = new Word.Builder();
+
+        Word word1 = builder
+                        .addLetter(new LetterUpperCase('H'))
+                        .addLetter(new LetterLowerCase('e'))
+                        .addLetter(new LetterLowerCase('l'))
+                        .addLetter(new LetterLowerCase('l'))
+                        .addLetter(new LetterLowerCase('o'))
+                        .build();
+
+        Word word2 = null;
+
+        boolean result = word1.equals(word2);
+
+        assertEquals(result, false);
     }
 }
